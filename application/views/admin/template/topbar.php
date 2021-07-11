3d7afbcd758cfe88c9e7abb106189729c8b4311f
rafifmulia <rafif.mulia.r@gmail.com>
2021-07-11 12:54:29 +0700

    change foto bg login, regis, forgot + fix get topbar avatar + folder uploads

--- application/views/admin/template/topbar.php
+++ application/views/admin/template/topbar.php
@@ -38,10 +38,13 @@
     <div class="topbar-divider d-none d-sm-block"></div>
 
     <!-- Nav Item - User Information -->
+    <?php
+      $user = $this->M_data->detail_user($this->session->userdata('id_user'))[0];
+    ?>
     <li class="nav-item dropdown no-arrow">
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $this->session->userdata('username') ?></span>
-        <img class="img-profile rounded-circle" src="<?= ($this->session->userdata('avatar') == null) ? base_url('assets/img/undraw_profile.svg') : base_url($this->session->userdata('avatar')) ?>">
+        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['username'] ?></span>
+        <img class="img-profile rounded-circle" src="<?= ($user['avatar'] == null) ? base_url('assets/img/undraw_profile.svg') : base_url($user['avatar']) ?>">
       </a>
       <!-- Dropdown - User Information -->
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
